55ad5fc3f9624e192c1570a946b9b56417bb950b
Burak Güner <burak.guner@callstack.com>
2023-12-24 17:02:26 +0300

    vec3 struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use image::RgbImage;
+use raytracing_in_one_weekend_rust::vec3::Vec3;
 
 const WIDTH: u32 = 1024;
 const HEIGHT: u32 = 1024;
@@ -7,12 +8,16 @@ fn main() {
     let mut img = RgbImage::new(WIDTH, HEIGHT);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let rel_x = x as f32 / WIDTH as f32;
-        let rel_y = y as f32 / HEIGHT as f32;
-        let r = (rel_x * 255.0) as u8;
-        let g = (rel_y * 255.0) as u8;
-        let b = (r / 2) + (g / 2);
-        *pixel = image::Rgb([r, g, b]);
+        let relative_x = x as f64 / WIDTH as f64;
+        let relative_y = y as f64 / HEIGHT as f64;
+
+        let color = Vec3 {
+            x: relative_x * 255.0,
+            y: relative_y * 255.0,
+            z: (relative_x * 255.0 / 3.0) + (relative_y * 255.0 / 3.0),
+        };
+
+        *pixel = color.into();
     }
 
     img.save("output.png").unwrap();
---
+++ src/vec3.rs
@@ -0,0 +1,72 @@
+use image::Rgb;
+
+#[derive(Debug, Clone)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vec3 {
+    pub fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+}
+
+impl Into<Rgb<u8>> for Vec3 {
+    fn into(self) -> Rgb<u8> {
+        Rgb([self.x as u8, self.y as u8, self.z as u8])
+    }
+}
+
+impl std::ops::Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl std::ops::Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl std::ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Self {
+        Vec3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl std::ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Vec3 {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+        }
+    }
+}
